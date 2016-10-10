66bf570de35d60edb1b4eed5bfbc671a144f501e
Julian Salinas <julian4001@outlook.com>
2016-10-09 20:56:43 -0600

    Me cago en la madre que pario al que invento github

--- src/Pruebas/Programa.scala
+++ src/Pruebas/Programa.scala
@@ -1,8 +1,7 @@
 package Pruebas
-import Clases.Matriz
 import Clases.Estado
 import Clases.Heuristica
-import Clases.solucion
+import Clases.Solucion
 
 
 object Programa {
@@ -10,7 +9,7 @@ object Programa {
   def main(args: Array[String]): Unit = {
     
     //Probando los metodos de la matriz
-    val matriz_1 = new Matriz(List(1,2,3,4,5,6,7,8,0))
+    val matriz_1 = new Estado(List(1,2,3,4,5,6,7,8,0))
     println("Matriz original: \n" + matriz_1)
     println("GetPosicion(6): " + matriz_1.GetPosicion(6))
     println("GetElemento(1,1): " + matriz_1.GetElemento(1, 1))
@@ -21,7 +20,7 @@ object Programa {
     //Pruebas de los estados
     
     //Estado raiz
-    val estado_1 = new Estado(null,List(1,2,3,4,0,6,7,8,5))
+    val estado_1 = new Estado(List(1,2,3,4,0,6,7,8,5))
     println("Estado raiz: \n" + estado_1)
     
     //Subestados de la raiz
@@ -30,7 +29,7 @@ object Programa {
       println("SubEstado "+ i + " de la raiz: \n"+ subEstados(i))
     
     //El estado anterior del primer subestado debe ser la raiz
-    val estado_anterior = subEstados(0).GetEstadoAnterior()
+    val estado_anterior = subEstados(0).Estado_Anterior
     println("Estado anterior al subEstado 0 de la raiz: \n" + estado_anterior)
     
     //SubEstados del subEstado 0 de la raiz
@@ -39,8 +38,8 @@ object Programa {
       println("SubEstado "+ i + " del subEstado 0: \n"+ subSubEstados(i))
     
    
-    val matriz1 = new    Matriz(List(2,12,14,13,7,3,9,5,8,6,11,10,0,4,15,1))
-    val matrizMeta = new Matriz(List(1,2,3,4,5,7,6,8,9,10,11,12,13,14,15,0))
+    val matriz1 = new Estado(List(2,12,14,13,7,3,9,5,8,6,11,10,0,4,15,1))
+    val matrizMeta = new Estado(List(1,2,3,4,5,7,6,8,9,10,11,12,13,14,15,0))
     
     println("\nMAT1")
     println(matriz1)
@@ -51,8 +50,8 @@ object Programa {
     
     println("SOLUBLE" + heuristicas.tieneSolucion(matriz1, matrizMeta))
     
-    val matrizComparar1 = new Matriz(List(1,2,3,4,5,6,7,8,9))
-    val matrizComparar2 = new Matriz(List(1,2,3,4,5,6,7,8,9))
+    val matrizComparar1 = new Estado(List(1,2,3,4,5,6,7,8,9))
+    val matrizComparar2 = new Estado(List(1,2,3,4,5,6,7,8,9))
     
     println(
         "\nmatrizComparar1.EqualsTo(matrizComparar2)\n"+ 
@@ -63,16 +62,16 @@ object Programa {
      */
     
     //tiene solucion
-    val m1 = new Matriz(List(2,1,3,4,5,6,8,7,0))
+    val m1 = new Estado(List(2,1,3,4,5,6,8,7,0))
     //no tiene solucion
-    val m2 = new Matriz(List(2,1,3,4,5,6,7,8,0))
+    val m2 = new Estado(List(2,1,3,4,5,6,7,8,0))
     
-    val s=new solucion()
+    val s = new Solucion()
     
-    println(s.tiene_solucion(m1))
+    println(s.HasSolucion(m1))
     
-    println(m1+"\ntiene solucion: "+s.tiene_solucion(m1))
-    println(m2+"\ntiene solucion: "+s.tiene_solucion(m2))
+    println(m1+"\ntiene solucion: "+s.HasSolucion(m1))
+    println(m2+"\ntiene solucion: "+s.HasSolucion(m2))
     
   }
   
