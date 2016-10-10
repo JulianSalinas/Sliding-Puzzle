685de1e6257ae0eae1623b12bd84730d61feb894
Brandon Dinarte <bdinarte@yahoo.com>
2016-10-09 19:59:47 -0600

    Heuristica de solubilidad para cualquier estado meta
    Hay que volarse el de olman equis de equis de

--- src/Clases/Heuristica.scala
+++ src/Clases/Heuristica.scala
@@ -9,10 +9,10 @@ class Heuristica {
    */
   def Manhattan( estadoPosible : Matriz , estadoMeta : Matriz ) : Int = {
     
-    var tamano: Int = estadoMeta.Dimension
+    var tamano: Int = estadoMeta.GetSecuencia().length
     var acumulado: Int = 0
     
-    for ( count <- 0 to Math.pow(tamano, 2).toInt) {
+    for ( count <- 0 to tamano-1) {
       val posible = estadoPosible.GetPosicion(count)
       val meta = estadoMeta.GetPosicion(count)
       // Peculiaridad de la heuristica
@@ -30,10 +30,10 @@ class Heuristica {
    */
   def TilesOutOf( estadoPosible : Matriz , estadoMeta : Matriz ) : Int = {
     
-    var tamano: Int = estadoMeta.Dimension
+    var tamano: Int = estadoMeta.GetSecuencia().length
     var acumulado: Int = 0
     
-    for ( count <- 0 to Math.pow(tamano, 2).toInt) {
+    for ( count <- 0 to tamano-1) {
       val posible = estadoPosible.GetPosicion(count)
       val meta = estadoMeta.GetPosicion(count)     
       // Peculiaridad de la heuristica
@@ -52,11 +52,11 @@ class Heuristica {
    */
   def MisplacedTiles( estadoPosible : Matriz , estadoMeta : Matriz ) : Int = {
     
-    var tamano: Int = estadoMeta.Dimension
+    var tamano: Int = estadoMeta.GetSecuencia().length
     var acumulado: Int = 0
     
     // Peculiaridad de la heuristica -> el for empieza en 1
-    for ( count <- 1 to Math.pow(tamano, 2).toInt) {
+    for ( count <- 0 to tamano-1) {
       val posible = estadoPosible.GetPosicion(count)
       val meta = estadoMeta.GetPosicion(count)      
       // Peculiaridad de la heuristica
@@ -65,5 +65,60 @@ class Heuristica {
     
     return acumulado
   }
-   
+  
+  /** 
+   *  Retorna true o false si es posible o no llegar desde el primer estado recibido hasta el 2do
+   *  @param estadoPosible estado inicial
+   *  @param estadoMeta estado al que se quiere llegar
+   */
+  def tieneSolucion( estadoPosible : Matriz, estadoMeta : Matriz ) : Boolean = {
+    
+    // algoritmo de esta heuristica
+    // https://www.cs.bham.ac.uk/~mdr/teaching/modules04/java2/TilesSolvability.html
+
+    var secuenciaMeta = estadoMeta.GetSecuencia()
+    var secuenciaPosible = estadoPosible.GetSecuencia()
+    var inversiones = 0
+    
+    var tuplasMeta : List[(Int,Int)] = List() 
+    
+    for (count <- 0 to secuenciaMeta.length-1) 
+      tuplasMeta = tuplasMeta:::List( (count, secuenciaMeta(count)) )
+    
+    // calcular numero de inversiones
+    for (elementCount <- 0 to secuenciaPosible.length-2) {
+      if (secuenciaPosible(elementCount) != 0)
+      for (indexCount <- elementCount+1 to secuenciaPosible.length-1){        
+        // valor relativo del elemento a verificar
+        if (secuenciaPosible(indexCount) != 0) {
+          val relativeVal = tuplasMeta.find(_._2 == secuenciaPosible(elementCount)).get._1
+          val relativePlus1 = tuplasMeta.find(_._2 == secuenciaPosible(indexCount)).get._1
+          
+          if (relativeVal > relativePlus1) inversiones += 1
+        }
+      }
+    }
+    
+    println("inversiones: " + inversiones)
+    var ancho = estadoMeta.Dimension
+    var anchoPar = ancho%2 == 0
+    var inversionesPar = inversiones%2 == 0
+    var blankEnPar =  (ancho - estadoPosible.GetPosicion(0)._1)%2 == 0
+    
+    if (!anchoPar && inversionesPar) {return true}
+    if (anchoPar && blankEnPar && !inversionesPar) {return true}
+    if (anchoPar && !blankEnPar && inversionesPar) {return true}
+    false
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 }
--- src/Pruebas/Programa.scala
+++ src/Pruebas/Programa.scala
@@ -39,8 +39,8 @@ object Programa {
       println("SubEstado "+ i + " del subEstado 0: \n"+ subSubEstados(i))
     
    
-    val matriz1 = new Matriz(List(2,8,7,4,5,6,3,0,1))
-    val matrizMeta = new Matriz(List(1,2,3,4,5,6,7,8,0))
+    val matriz1 = new    Matriz(List(2,12,14,13,7,3,9,5,8,6,11,10,0,4,15,1))
+    val matrizMeta = new Matriz(List(1,2,3,4,5,7,6,8,9,10,11,12,13,14,15,0))
     
     println("\nMAT1")
     println(matriz1)
@@ -49,10 +49,11 @@ object Programa {
     
     val heuristicas = new Heuristica()
     
-    println("\n" + heuristicas.MisplacedTiles(matriz1, matrizMeta))
+    println("SOLUBLE" + heuristicas.tieneSolucion(matriz1, matrizMeta))
     
     val matrizComparar1 = new Matriz(List(1,2,3,4,5,6,7,8,9))
     val matrizComparar2 = new Matriz(List(1,2,3,4,5,6,7,8,9))
+    
     println(
         "\nmatrizComparar1.EqualsTo(matrizComparar2)\n"+ 
         matrizComparar1.EqualsTo(matrizComparar2)
